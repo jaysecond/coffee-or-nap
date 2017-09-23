f85082be538a9a236d28313805b215f77ddb4ae9
Jay Second <Jay Second>
2017-09-22 23:24:05 -0700

    adding a second page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,5 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
-  
+  page = 0;
+
+  public next = () => {
+    this.page++;
+    return this.page;
+  }
+
+  public previous = () => {
+    this.page--;
+    return this.page;
+  }  
 }
