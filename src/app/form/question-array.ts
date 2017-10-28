b0eda1bdd0153ab5fed76ba8efe139390642c1d0
April Jackson <april.jackson012@gmail.com>
2017-10-27 23:08:23 -0700

    Update option next values

--- src/app/form/question-array.ts
+++ src/app/form/question-array.ts
@@ -5,54 +5,52 @@ export const QUESTIONS: Question[] = [
         id: 0,
         question: "How much sleep did you get last night?",
         options: [
-            { option: "0 hours", next: 0 },
-            { option: "Less than 4 hours", next: 0.33 },
-            { option: "4-7 hours", next: 0.66 },
-            { option: "7 or more hours", next: 1 }
+            { option: "0 hours", next: 1 },
+            { option: "Less than 4 hours", next: 1 },
+            { option: "4-7 hours", next: 3 },
+            { option: "7 or more hours", next: 3 }
         ]
     },
     {
         id: 1,
         question: "When is your next activity?",
         options: [
-            { option: "Right now", next: 1 },
-            { option: "In less than 30 minutes", next: 0.66 },
-            { option: "In 30 minutes - 1.5 hours", next: 0.33 },
-            { option: "In more than 1.5 hours", next: 1 }
+            { option: "Right now", next: 2 },
+            { option: "In less than 30 minutes", next: 9 },
+            { option: "In more than 30 mins", next: 7 }
         ]
     },
     {
         id: 2,
         question: "Can you sleep during your next activity?",
         options: [
-            { option: "Yes", next: 0 },
-            { option: "No", next: 1 }
+            { option: "Yes", next: 7 },
+            { option: "No", next: 8 }
         ]
     },
     {
         id: 3,
         question: "How much coffee have you had today?",
         options: [
-            { option: "None", next: 1 },
-            { option: "1-2 cups", next: 0.66 },
-            { option: "3-5 cups", next: 0.33 },
-            { option: "More than 5 cups", next: 0 }
+            { option: "None", next: 8 },
+            { option: "1-3 cups", next: 4 },
+            { option: "More than 3 cups", next: 1 }
         ]
     },
     {
         id: 4,
         question: "Do you like coffee?",
         options: [
-            { option: "Yes", next: 1 },
-            { option: "No", next: 0 }
+            { option: "Yes", next: 8 },
+            { option: "No", next: 5 }
         ]
     },
     {
         id: 5,
         question: "Do you like naps?",
         options: [
-            { option: "Yes", next: 0 },
-            { option: "No", next: 1 }
+            { option: "Yes", next: 1 },
+            { option: "No", next: 10 }
         ]
     }
 ];
